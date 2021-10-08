bdfac67fd15c72437fbf4a671ab06099ab1dfe28
jaysenSAW <49760854+jaysenSAW@users.noreply.github.com>
2021-10-08 13:08:07 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -15,19 +15,58 @@ from plotly.subplots import make_subplots
 import plotly.graph_objs as go
 import os
 import joblib
+import glob
+import shap
+import pickle
+import matplotlib.pyplot as plt
+
+
 
 # load the model from disk
-mdl = joblib.load("tree_decision")
+global mdl
+mdl = pickle.load(open("data/tree_decision", "rb"))
+global explainer
+explainer = pickle.load(open("data/explainer_shap_tree", "rb"))
+
+global feat
+feat = ['AMT_INCOME_TOTAL',
+ 'AMT_ANNUITY',
+ 'AGE',
+ 'YEARS_EMPLOYED',
+ 'OWN_CAR_AGE',
+ 'CREDIT_active',
+ 'CREDIT_MEAN_OVERDUE_active',
+ 'CREDIT_MEAN_active',
+ 'CREDIT_MEAN_OVERDUE_closed',
+ 'CREDIT_MEAN_closed',
+ 'proportion_OVERDUE_closed',
+ 'CREDIT_ask',
+ 'Number_years_Loan_Theorical',
+ 'INCOME_ANNUITY_RATIO',
+ 'CNT_FAM_MEMBERS']
 #clean folder
 #if os.path.isfile("data/customer_info.csv"):
 #    os.remove("data/customer_info.csv")
 #    print("File Removed!")
 
 global df
+global shap_values
 df = pd.read_csv('data/clean_data2.csv')
 df = df.sample(n=5000, random_state=42)
-global dt
-dt = None
+
+Xtest = df[feat].to_numpy()
+shap_values = explainer.shap_values(Xtest, check_additivity=False)
+figure = plt.gcf()
+#shap.summary_plot(shap_values, Xtest, feature_names=feat, show=False)
+shap.summary_plot(shap_values, Xtest, feature_names=feat, show=False)
+figure.set_size_inches(8, 8)
+plt.savefig(fname = "static/temp.jpg", dpi = 100, pad_inches = 0, bbox_inches='tight')
+plt.close()
+
+global url
+url = "http://localhost:5000/"
+global new_url
+new_url = "http://jaysen.pythonanywhere.com/"
 
 class ContactForm(Form):
     Credit_ask = FloatField("Credit ask", validators=[NumberRange(min=0,
@@ -40,17 +79,53 @@ class ContactForm(Form):
     YEARS_EMPLOYED = FloatField("Years of Employment")
     CNT_FAM_MEMBERS = FloatField("Number of familly")
     CREDIT_active = FloatField("Number of current credit")
+    CREDIT_MEAN_OVERDUE_active = FloatField("Overdue amount for current credit")
     CREDIT_MEAN_active = FloatField("Amount for current credit")
     CREDIT_MEAN_closed = FloatField("Mean amount for closed credit")
     CREDIT_MEAN_OVERDUE_closed = FloatField("Overdue amount for closed credit")
     proportion_OVERDUE_closed = FloatField("Ratio of credit with overdue",
     validators=[NumberRange(min=0, max=1, message='Number between  0 and 1 is \
     expected Ratio of credit with overdue')])
+    #env = os.getcwd()
+    #print(env)
+    #if env.split("\")[2] == 'jayse':
+    #    env='http://localhost:5000/contact'
+    #else:
+    #    env='http://jaysen.pythonanywhere.com/contact'
     submit = SubmitField("Send")
 
 
 app = Flask(__name__)
 app.secret_key = 'development key'
+
+print("!!!!!!!!!!!!!!!!!!!!")
+env = os.getcwd()
+print(env)
+print("!!!!!!!!!!!!!!!!!!!!")
+
+
+def change_url(file):
+    tmp = ""
+    with open(file, "r") as lines:
+        for line in lines:
+            if len(line.split()) != 7:
+                tmp += line
+            elif line.split("\"")[0].replace(" ","") == '<formaction=':
+                tmp += line.replace(url, new_url)
+    with open(file, "w") as filout:
+        filout.write(tmp)
+
+
+def init_url():
+    print("!!!!!!!!!!!!!!!!!!!!")
+    env = os.getcwd()
+    if env.split('\\')[2]  == "jayse":
+        print("No change")
+    else:
+        files = glob.glob("templates/*.html")
+        for file in files:
+            change_url(file)
+
 
 def predict_credit(data):
     AMT_income = np.log10(float(data['AMT_income']) + 1)
@@ -63,16 +138,17 @@ def predict_credit(data):
     Employed = float(data['YEARS_EMPLOYED'])
     Age_car = float(data['Age_car'])
     CREDIT_active = float(data['CREDIT_active'])
+    CREDIT_MEAN_OVERDUE_active = np.log10(float(data['CREDIT_MEAN_active']) + 1)
     CREDIT_MEAN_active = np.log10(float(data['CREDIT_MEAN_active']) + 1)
     CREDIT_MEAN_OVERDUE_closed = float(data['CREDIT_MEAN_OVERDUE_closed'])
     CREDIT_MEAN_closed = np.log10(float(data['CREDIT_MEAN_closed']) + 1)
     proportion_OVERDUE_closed = float(data['proportion_OVERDUE_closed'])
     CNT_FAM_MEMBERS = float(data['CNT_FAM_MEMBERS'])
-
+    global dt
     dt = np.array([AMT_income, Loan_annuity, Age, Employed, Age_car,
-    CREDIT_active, CREDIT_MEAN_active, CREDIT_MEAN_OVERDUE_closed,
-    CREDIT_MEAN_closed, proportion_OVERDUE_closed, Credit_ask, Years_refund,
-    INCOME_ANNUITY_RATIO, CNT_FAM_MEMBERS])
+    CREDIT_active, CREDIT_MEAN_OVERDUE_active, CREDIT_MEAN_active,
+    CREDIT_MEAN_OVERDUE_closed, CREDIT_MEAN_closed, proportion_OVERDUE_closed,
+    Credit_ask, Years_refund, INCOME_ANNUITY_RATIO, CNT_FAM_MEMBERS])
     print(dt)
     y_pred = mdl.predict(dt.reshape(1, -1))
     print("Result for credit {0}".format(y_pred))
@@ -82,6 +158,7 @@ def predict_credit(data):
     "YEARS_EMPLOYED" : [Employed],
     "OWN_CAR_AGE" : [Age_car],
     "CREDIT_active" : [CREDIT_active],
+    "CREDIT_MEAN_OVERDUE_active": [float(CREDIT_MEAN_OVERDUE_active)],
     "CREDIT_MEAN_active" : [float(data['CREDIT_MEAN_active'])],
     "CREDIT_MEAN_OVERDUE_closed" : [CREDIT_MEAN_OVERDUE_closed],
     "CREDIT_MEAN_closed" : [float(data['CREDIT_MEAN_closed'])],
@@ -92,29 +169,21 @@ def predict_credit(data):
     "CNT_FAM_MEMBERS" : [CNT_FAM_MEMBERS] }
     dt = pd.DataFrame(data)
     dt.to_csv("data/customer_info.csv")
+    ###################################
+    Xtest2 = dt.to_numpy()
+    #shap.initjs()
+    #figure = plt.gcf()
+    shap_values2 = explainer.shap_values(dt.to_numpy())
+    shap.force_plot(
+    explainer.expected_value, shap_values2[0,:], Xtest2[0,:],
+    feature_names=dt.columns, show=False, matplotlib=True
+    ).savefig('static/temp2.jpg')
+    #shap.summary_plot(shap_values, dt.to_numpy(), feature_names=feat, show=False)
+    #figure.set_size_inches(8, 8)
+    #plt.savefig(fname = "static/temp2.jpg", dpi = 100, pad_inches = 0, bbox_inches='tight')
+    #plt.close()
     return y_pred[0]
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/contact', methods = ['GET', 'POST'])
-def contact():
-    form = ContactForm()
-    if request.method == 'POST':
-        if form.validate() == False:
-            flash('All fields are required.')
-            return render_template('question.html', form = form)
-        else:
-            print("ok")
-            data = request.form.to_dict()
-            print(data)
-            if predict_credit(data) == 1:
-                return render_template('prediction_refused.html', form = form)
-            else:
-                return render_template('prediction_accepted.html', form = form)
-    elif request.method == 'GET':
-        return render_template('question.html', form = form)
 
 
 def pieplot_familly():
@@ -149,8 +218,8 @@ def hist_income():
         name='Accepted'))
     # Overlay both histograms
     fig.update_layout(barmode='overlay',
-                     title_text="Income annuity ratio's density plot",
-                     xaxis_title_text='Income annuity ratio',
+                     title_text="Income annuity density plot",
+                     xaxis_title_text='Income annuity',
                      yaxis_title_text= 'Probabily density')
     # Reduce opacity to see both histograms
     fig.update_traces(opacity=0.7)
@@ -159,7 +228,7 @@ def hist_income():
 def plot_ratio():
         fig = px.scatter(df, x = "AMT_ANNUITY" , y = "INCOME_ANNUITY_RATIO", color="TARGET")
         fig.update_traces(opacity=0.7)
-        fig.show()
+        #fig.show()
         # Overlay both histograms
         fig.update_layout(title_text="Income vs Income annuity ratio",
                          xaxis_title_text='Annuity loan',
@@ -174,11 +243,37 @@ def plot_ratio():
         fig.update_traces(opacity=0.7)
         return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
+@app.route('/')
+def index():
+    #init_url()
+    return render_template('index.html')
+
+
+
+
+@app.route('/contact', methods = ['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash('All fields are required.')
+            return render_template('question.html', form = form)
+        else:
+            print("ok")
+            data = request.form.to_dict()
+            print(data)
+            if predict_credit(data) == 1:
+                return render_template('prediction_refused.html', form = form)
+            else:
+                return render_template('prediction_accepted.html', form = form)
+    elif request.method == 'GET':
+        return render_template('question.html', form = form)
+
 
 @app.route('/familly')
 def chart2():
     graphJSON = pieplot_familly()
-    header="Familly member"
+    header="Number of familly member"
     if dt is None:
         description = """
         Credit's criterions depands on many features these charts show differences between accepted and rejected credits.
@@ -187,7 +282,6 @@ def chart2():
         description = "Credit's criterions depands on many features these charts show differences between accepted and rejected credits.\
 In according to form customer has {0} member(s).".format(dt["CNT_FAM_MEMBERS"])
     return render_template('summary.html', graphJSON=graphJSON, header=header,description=description)
-
 
 
 @app.route('/income')
@@ -209,13 +303,28 @@ def income_loan_ratio():
 def summary_data_info():
     graphJSON = pieplot_familly()
     header="Data summary"
-    description1 = "Rejected loan are more present for familly with more than 4 member"
     histJSON = hist_income()
     description1 = "Income "
     scatterJSON = plot_ratio()
-    description2 = "Ratio income/loan annuity is important to determine if candidate can pay its loans"
-    return render_template('summary_data.html', graphJSON=[graphJSON, scatterJSON, histJSON], header=header,description=description1)
+    if os.path.isfile("data/customer_info.csv"):
+        print("add customer point !")
+        new_dt = pd.read_csv('data/customer_info.csv')
+        description1 = "Rejected loan are more present for \
+        familly with more than 4 member. You have {0:.1f} members".format(new_dt.loc[0, "CNT_FAM_MEMBERS"])
+        description2 = "Ratio income/loan annuity is important to determine \
+        if candidate can pay its loans. Your ratio is {0:.2f}".format(new_dt.loc[0,"INCOME_ANNUITY_RATIO"])
+        description3 = "Income distribution for accepted and rejected candidats.\
+        Your income is {0:.2f}".format(new_dt.loc[0,"AMT_INCOME_TOTAL"])
+    else:
+        description1 = "Rejected loan are more present for familly with more than 4 member"
+        description2 = "Ratio income/loan annuity is important to determine if candidate can pay its loans"
+        description3 = "Income distribution for accepted and rejected candidats"
+    return render_template('summary_data.html', graphJSON=[graphJSON, scatterJSON, histJSON], header=header,description=[description1, description2, description3])
 
+
+@app.route("/test_shap")
+def cusomer_data():
+    return render_template('index.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
